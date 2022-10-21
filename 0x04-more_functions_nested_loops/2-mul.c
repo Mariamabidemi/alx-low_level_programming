2eba5f0bfa55af468b8a30cdc82de2f6d1e56a31
mariamabidemi <mariamabidemi255@gmail.com>
2022-10-20 21:14:20 -0700

    first nested commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#insert "main.h"
+
+/**
+ * mul - multiply tow integers
+ * @a: first int
+ * @b: second int
+ * Return: the multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
